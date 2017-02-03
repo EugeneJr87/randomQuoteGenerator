04737f4931d802afddaff5bf035784d5232a0ec6
Eugene Liva Jr <eugenelivajr@gmail.com>
2017-02-03 16:54:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,18 @@
-var quotes = ["A mother’s love for her child is like nothing else in the world. It knows no aw, no pity, it dares all things and crushes down remorselessly all that stands in its path.",
- 			  "Family quarrels are bitter things. They don’t go according to any rules. They’re not like aches or wounds, they’re more like splits in the skin that won’t heal because there’s not enough material.",
- 			  "The best preparation for tomorrow is doing your best today.",
- 			  "Always do your best. What you plant now, you will harvest later.",
- 			  "The best thing to hold onto in life is each other." 
-]
+function getNewQuote(){
 
-
-var author = ["Agatha Christie",
-			  "F. Scott Fitzgerald",
-			  "H. Jackson Brown, Jr.",
-			  "Og Mandino",
-			  "Audrey Hepburn"
-]
-
-
-function getRandomQuotes(){
-
-	var newQuotes = Math.floor(Math.random() * quotes.length);
-	var authors = newQuotes;
-
-	var newerQuotes = $("#quote").text(quotes[newQuotes]);
-	var newerAuthors = $("#author").text(dash + author[authors]);
+	var request = $.ajax({
+   	url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?",
+   	dataType: "jsonp"
+ });
+ 	request.done(function(result) {
+ 		$("#quote").html(result[0].content);
+ 		$("#author").text(result[0].title);
+	});
 
 }
 
 $("#new").on("click", function(){
-	getRandomQuotes()
-	$(this).css('background-image', 'url(https://source.unsplash.com/random)');
-
+	getNewQuote();
 });
 
-$("#quote").fadeIn(1000);
+getNewQuote();
